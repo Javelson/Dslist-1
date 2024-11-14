cb500a54931ae225d5e33ad8d0d6a66c4d51ff5a
Javelson Canzenze <javelsoncanzenze7@gmail.com>
2024-11-14 13:42:55 +0100

    Game send

--- src/main/java/com/javdev/dslist/entities/Game.java
+++ src/main/java/com/javdev/dslist/entities/Game.java
@@ -21,7 +21,9 @@ public class Game {
 
     private String genre;
 
-    private String platform; // Corrigido de 'plataform' para 'platform'
+    private String platforms; // Corrigido de 'plataform' para 'platform'
+
+    private Double score;
 
     private String imgUrl;
 
@@ -34,19 +36,92 @@ public class Game {
     public Game() {
     }
 
-    public Game(long id, String title, Integer year, String genre, String platform, String imgUrl,
+    public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
                 String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
 
-    // Getters e setters...
+     
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatform() {
+        return platforms;
+    }
+
+    public void setPlatform(String platform) {
+        this.platforms = platform;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
 
     @Override
     public int hashCode() {
